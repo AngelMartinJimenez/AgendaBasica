1646e22da4bdc3074140f0bf3f519cc4b36426b9
AngelMartinJimenez <45063649+AngelMartinJimenez@users.noreply.github.com>
2018-12-05 16:21:18 +0100

    inicio

---
+++ src/main/java/com/agenda/app/AgendaBasica/AgendaBasicaApplication.java
@@ -0,0 +1,12 @@
+package com.agenda.app.AgendaBasica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaBasicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaBasicaApplication.class, args);
+	}
+}
