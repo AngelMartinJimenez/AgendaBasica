f329cd04a7b5e0e9c330a35cfb59128be89c9938
MVictoriaPov <45061787+MVictoriaPov@users.noreply.github.com>
2018-12-06 13:50:57 +0100

    modificacion en properties del nombre de schema en la BBDD y prueba fallida en el main

--- src/main/java/com/agenda/app/AgendaBasica/AgendaBasicaApplication.java
+++ src/main/java/com/agenda/app/AgendaBasica/AgendaBasicaApplication.java
@@ -3,10 +3,15 @@ package com.agenda.app.AgendaBasica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import modelo.Persona;
+
 @SpringBootApplication
 public class AgendaBasicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaBasicaApplication.class, args);
+		
+		
 	}
 }
