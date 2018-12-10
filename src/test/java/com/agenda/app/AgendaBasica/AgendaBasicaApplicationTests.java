cf258df767d957be5db22b2cb8a22e971612b13a
roarra <43341488+roarra@users.noreply.github.com>
2018-12-10 17:13:21 +0100

    prueba webLayer

--- src/test/java/com/agenda/app/AgendaBasica/AgendaBasicaApplicationTests.java
+++ src/test/java/com/agenda/app/AgendaBasica/AgendaBasicaApplicationTests.java
@@ -4,12 +4,21 @@ package com.agenda.app.AgendaBasica;
  * @author: María victoria
  */
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 import com.agenda.app.AgendaBasica.control.AgendaController;
 
 /*
@@ -32,5 +41,22 @@ public class AgendaBasicaApplicationTests {
         //Para asegurar que falla elimina la linea del controlador
         //    @Controller
     }
+	
+	@RunWith(SpringRunner.class)
+	@WebMvcTest
+	public class ApplicationTest05_webLayer {
 
+	    @Autowired
+	    private MockMvc mockMvc;
+
+	    @Test
+	    public void webLayer() throws Exception {
+	        this.mockMvc
+	        .perform(get("/"))
+	        .andDo(print())
+	        .andExpect(status().isOk())
+	        .andExpect(content().string(containsString("Hello World")));
+	    }
+
+	}
 }
