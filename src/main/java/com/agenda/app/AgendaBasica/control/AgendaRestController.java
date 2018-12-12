2d2748867d042f242c120b22d60ee82323c5ff27
roarra <43341488+roarra@users.noreply.github.com>
2018-12-12 12:41:12 +0100

    deleterest

--- src/main/java/com/agenda/app/AgendaBasica/control/AgendaRestController.java
+++ src/main/java/com/agenda/app/AgendaBasica/control/AgendaRestController.java
@@ -2,6 +2,7 @@ package com.agenda.app.AgendaBasica.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,13 @@ public class AgendaRestController {
 		
 		
 		return personaServicios.create(persona);
+	}
+	
+	@DeleteMapping
+	
+	public Persona deletePerson(@RequestBody int id) {
+		
+		return personaServicios.deletePerson(id);
 	}
 	
 	
