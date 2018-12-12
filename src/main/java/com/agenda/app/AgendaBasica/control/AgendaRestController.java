2d7bd142ceb11fd9a10534551196cb180ade444c
roarra <43341488+roarra@users.noreply.github.com>
2018-12-12 17:12:04 +0100

    readRest

--- src/main/java/com/agenda/app/AgendaBasica/control/AgendaRestController.java
+++ src/main/java/com/agenda/app/AgendaBasica/control/AgendaRestController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.agenda.app.AgendaBasica.modelo.Persona;
 import com.agenda.app.AgendaBasica.servicios.IPersonaS;
+
 
 
 
@@ -61,7 +63,10 @@ public class AgendaRestController {
         return personaServicios.updatePerson(id,persona);
     }
 	
-	
+	@GetMapping(path = {"/{id}"})
+    public Persona readPerson(@PathVariable("id") int id){
+        return personaServicios.read(id);
+    }
 	
 	
 	
