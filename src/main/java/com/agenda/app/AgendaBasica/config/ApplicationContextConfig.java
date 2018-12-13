6d80199d17aa46e28ba69e269e72e3fa57ed0b9e
AnaMariaFer <45061776+AnaMariaFer@users.noreply.github.com>
2018-12-13 15:06:33 +0100

    SWAGGEEEEEER <3

--- src/main/java/com/agenda/app/AgendaBasica/config/ApplicationContextConfig.java
+++ src/main/java/com/agenda/app/AgendaBasica/config/ApplicationContextConfig.java
@@ -1,36 +1,47 @@
-/*package com.agenda.app.AgendaBasica.config;
+package com.agenda.app.AgendaBasica.config;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@ComponentScan("com.agenda.app.AgendaBasica")
-@EnableWebMvc
-@EnableTransactionManagement
+@EnableSwagger2
+
+
 public class ApplicationContextConfig {
-    @Bean(name = "viewResolver")
-    public InternalResourceViewResolver getViewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
-    
 
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");   
-	}
-     
-   
+	 @Bean
+		public Docket productApi() {
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.any())
+					.paths(PathSelectors.any())
+					.build();
+		}
+	
+	
+	 public void addViewControllers(ViewControllerRegistry registry) {         
+		 
+		 registry.addRedirectViewController("/docApi/v2/api-docs", "/v2/api-docs");         
+		 registry.addRedirectViewController("/docApi/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");         
+		 registry.addRedirectViewController("/docApi/swagger-resources/configuration/security", "/swagger-resources/configuration/security");        
+		 registry.addRedirectViewController("/docApi/swagger-resources", "/swagger-resources");     }         
+	 
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) {          
+		
+		 registry.addResourceHandler("/docApi/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");            
+		 registry.addResourceHandler("/docApi/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");              }
+	
 
-}*/
+}
 
 
 //no hace falta el spring boot lo hace el solo
